616edf0c3dd89e8443916ad3903d11f706a766fd
Marine67 <gopi.fullstackdev@gmail.com>
2025-10-28 14:11:27 +0530

    Update hc int 5

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public string test;
 
-        public int a;
+        public int a=10;
 
         public HomeController(ILogger<HomeController> logger, DBContext context, IConfiguration configuration)
         {
